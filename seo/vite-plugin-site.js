a49e72b81123714eec18e65a384f4165c541aa37
Ryhox <emanuelpfeifer1@gmail.com>
2026-09-19 12:30:40 +0200

    fix

--- seo/vite-plugin-site.js
+++ seo/vite-plugin-site.js
@@ -258,16 +258,11 @@ function sitemap() {
     `    <lastmod>${today()}</lastmod>`,
     `    <changefreq>${p.changefreq}</changefreq>`,
     `    <priority>${p.priority}</priority>`,
-    ...(p.path === '/' ? [
-      '    <image:image>',
-      `      <image:loc>${abs('/og-image.jpg')}</image:loc>`,
-      '    </image:image>',
-    ] : []),
     '  </url>',
   ].join('\n'));
   return [
     '<?xml version="1.0" encoding="UTF-8"?>',
-    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">',
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
     ...urls,
     '</urlset>',
     '',
